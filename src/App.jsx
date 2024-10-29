a1d91796057df4ed5f8fe205bac36275f4a78736
Manuel <manuelxgabriel@gmail.com>
2024-10-28 23:16:23 -0700

    completed the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Header from '/src/sections/Header.jsx'
+import React from "react";
+import Header from "/src/sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
-    return (
-        <main className="overflow-hidden">
-            <Header/>
-        </main>
-    )
-}
-export default App
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+export default App;
