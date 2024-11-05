0078dbe5f6e4f565a8a67d33f3442636d29827e4
Manuel <manuelxgabriel@gmail.com>
2024-11-04 22:42:28 -0800

    Finished the feature section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section
       className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36
-    max-md:pt-36 max-md:pb-32 border-2 border-amber-400"
+    max-md:pt-36 max-md:pb-32 "
     >
       <Element name="hero">
         <div className="container">
